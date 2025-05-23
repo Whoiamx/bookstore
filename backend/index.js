88d1dd6d40fafd3fd01d3ac1f13ab5f265de8348
Whoiamx <gaastontimchuk@gmail.com>
2025-05-23 12:56:10 -0300

    add changes

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,31 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
-const books = require("./src/booksForSale.json");
+const fs = require("fs");
 
 const app = express();
 const port = 4848;
 
 app.use(cors());
-
 app.use("/assets", express.static(path.join(__dirname, "src", "assets")));
 
 app.get("/books", (req, res) => {
-  res.send(books);
+  const filePath = path.join(__dirname, "src", "booksForSale.json");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error leyendo JSON:", err);
+      return res.status(500).json({ error: "Error leyendo archivo JSON" });
+    }
+
+    try {
+      const books = JSON.parse(data);
+      res.json(books);
+    } catch (parseError) {
+      console.error("Error parseando JSON:", parseError);
+      res.status(500).json({ error: "Error parseando JSON" });
+    }
+  });
 });
 
 app.listen(port, () => {

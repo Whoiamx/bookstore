9821b2ffc96efbb170f7773f147ea99a77e39e12
Whoiamx <gaastontimchuk@gmail.com>
2025-05-25 17:12:53 -0300

    better color palette

--- frontend/app/components/navbar/Navbar.tsx
+++ frontend/app/components/navbar/Navbar.tsx
@@ -8,10 +8,10 @@ import Link from "next/link";
 export const Navbar = () => {
   return (
     <>
-      <div className=" flex items-center p-3 justify-around bg-blue-400">
+      <div className=" flex items-center p-3 justify-around bg-[#002447]">
         <div className="flex items-center justify-center gap-2">
           <Link href={"/"}>
-            <h1 className="font-extrabold text-3xl">
+            <h1 className=" text-white font-extrabold text-3xl">
               Book<span className="text-blue-900">S</span>tore
             </h1>
           </Link>
@@ -26,17 +26,28 @@ export const Navbar = () => {
         </div>
         <div className="flex gap-8 justify-around items-center">
           <Link href={"/auth/login"}>
-            <FaUser style={{ fontSize: 25, cursor: "pointer" }} />
+            <FaUser
+              style={{ color: "white", fontSize: 25, cursor: "pointer" }}
+            />
           </Link>
           <Link
             href={"https://api.whatsapp.com/send/?phone=5491163099115"}
             target="_blank"
           >
-            <BsWhatsapp style={{ fontSize: 25, cursor: "pointer" }} />
+            <BsWhatsapp
+              style={{ color: "white", fontSize: 25, cursor: "pointer" }}
+            />
           </Link>
-          <Link href={"/cart"}>
-            <LuShoppingCart style={{ fontSize: 25, cursor: "pointer" }} />
-          </Link>
+          <div className="flex gap-3 justify-center items-center">
+            <Link href={"/cart"}>
+              <LuShoppingCart
+                style={{ color: "white", fontSize: 25, cursor: "pointer" }}
+              />
+            </Link>
+            <p className="bg-blue-200 w-6 p-1.5 rounded-2xl  text-center text-lg font-extrabold">
+              1
+            </p>
+          </div>
         </div>
       </div>
       <div>

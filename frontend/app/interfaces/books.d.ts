11a22377e718afbb5f540e4e0d11ee11731511b3
Whoiamx <gaastontimchuk@gmail.com>
2025-05-31 18:03:08 -0300

    persist localstorage

--- frontend/app/interfaces/books.d.ts
+++ frontend/app/interfaces/books.d.ts
@@ -21,3 +21,13 @@ export interface Producto {
   cantidad?: number;
   precio?: number;
 }
+
+export interface DirectionUser {
+  name: string;
+  apellido: string;
+  direction: string;
+  ciudad: string;
+  provincia: string;
+  postal: number;
+  telefono: number;
+}
--- frontend/app/store/store.ts
+++ frontend/app/store/store.ts
@@ -1,61 +1,66 @@
 import { create } from "zustand";
-import { Books } from "../interfaces/books";
+import { persist } from "zustand/middleware";
+import { Books, DirectionUser } from "../interfaces/books";
 
 interface StateStore {
   cart: Books[];
-  directionUser: {
-    name: string;
-    apellido: string;
-    direction: string;
-    ciudad: string;
-    provincia: string;
-    postal: number;
-    telefono: number;
-  };
+  directionUser: DirectionUser;
 
   addToCart: (book: Books) => void;
   deleteBookCart: (book: Books) => void;
 }
 
-export const useBookStore = create<StateStore>((set) => ({
-  cart: [],
-  directionUser: {
-    name: "",
-    apellido: "",
-    direction: "",
-    ciudad: "",
-    provincia: "",
-    postal: 0,
-    telefono: 0,
-  },
+export const useBookStore = create<StateStore>()(
+  persist(
+    (set) => ({
+      cart: [],
+      directionUser: {
+        name: "",
+        apellido: "",
+        direction: "",
+        ciudad: "",
+        provincia: "",
+        postal: 0,
+        telefono: 0,
+      },
 
-  addToCart: (book: Books) => {
-    set((state) => {
-      const existingBook = state.cart.find((item) => item.slug === book.slug);
+      addToCart: (book: Books) => {
+        set((state) => {
+          const existingBook = state.cart.find(
+            (item) => item.slug === book.slug
+          );
+          const incomingCantidad = book.cantidad ?? 1;
 
-      const incomingCantidad = book.cantidad ?? 1;
+          if (existingBook) {
+            const updatedCart = state.cart.map((item) =>
+              item.slug === book.slug
+                ? { ...item, cantidad: (item.cantidad ?? 1) + incomingCantidad }
+                : item
+            );
+            return { cart: updatedCart };
+          } else {
+            return {
+              cart: [...state.cart, { ...book, cantidad: incomingCantidad }],
+            };
+          }
+        });
+      },
 
-      if (existingBook) {
-        const updatedCart = state.cart.map((item) =>
-          item.slug === book.slug
-            ? { ...item, cantidad: (item.cantidad ?? 1) + incomingCantidad }
-            : item
-        );
-
-        return { cart: updatedCart };
-      } else {
-        return {
-          cart: [...state.cart, { ...book, cantidad: incomingCantidad }],
-        };
-      }
-    });
-  },
-
-  deleteBookCart: (book: Books) => {
-    set((state) => {
-      const filterCart = state.cart.filter((item) => item.slug != book.slug);
-
-      return { cart: filterCart };
-    });
-  },
-}));
+      deleteBookCart: (book: Books) => {
+        set((state) => {
+          const filterCart = state.cart.filter(
+            (item) => item.slug !== book.slug
+          );
+          return { cart: filterCart };
+        });
+      },
+    }),
+    {
+      name: "book-store", // Nombre de la clave en localStorage
+      partialize: (state) => ({
+        cart: state.cart,
+        directionUser: state.directionUser,
+      }),
+    }
+  )
+);

66cdee6fc079b5529714640e5ea2c2fe51ae512e
Whoiamx <gaastontimchuk@gmail.com>
2025-06-07 23:25:15 -0300

    fix

--- frontend/app/(shop)/product/[slug]/page.tsx
+++ frontend/app/(shop)/product/[slug]/page.tsx
@@ -1,14 +1,7 @@
-// app/(shop)/product/[slug]/page.tsx  <-- servidor (server component)
 import { Books } from "@/app/interfaces/books";
 import { Footer } from "@/app/components/footer/Footer";
 import { Navbar } from "@/app/components/navbar/Navbar";
 import ClientProductDetail from "@/app/components/ClientProductDetail";
-
-interface Props {
-  params: {
-    slug: string;
-  };
-}
 
 export async function generateStaticParams() {
   const res = await fetch("http://localhost:3232/books");
@@ -19,7 +12,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function Page({ params }: Props) {
+export default function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
 
   return (
